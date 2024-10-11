6ae42db0ea5f9a89e3179b9b50bbf1790828422f
Izalhadji31 <ryzalhadji@gmail.com>
2024-10-11 20:01:24 +0700

    membuat ContohList pada Collections.kt

--- app/src/main/java/com/example/kotlin/Collections.kt
+++ app/src/main/java/com/example/kotlin/Collections.kt
@@ -1,2 +1,11 @@
 package com.example.kotlin
 
+//List adalah kumpulan data yang disusun secara berurutan dari
+//List bersifat ordered , artinya data yang di masukkan akan memiliki posisi sesuai urutan
+//List bersifat muteable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat di ubah
+//list read only menggunakan listof
+// list muteable menggunakan muteablelistof
+
+fun ContohList() {
+    println("=== List ===")
+}
