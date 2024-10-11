241e1a303863e9729a9c6d3de7c08799291c0751
Izalhadji31 <ryzalhadji@gmail.com>
2024-10-11 19:55:59 +0700

    memasukan contac id 1, dan email ijal@gmail.com

--- app/src/main/java/com/example/kotlin/Class.kt
+++ app/src/main/java/com/example/kotlin/Class.kt
@@ -5,5 +5,8 @@ class Motor()
 class Contact(val id: Int,var email: String)
 
 fun main() {
+    val contact = Contact(1,"ijal@gmail.com")
 
+    //prints the value of the property: email
+    println(contact.email)
 }
